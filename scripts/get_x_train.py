7e519a63d37f7d198883c93fb99d7450d0e694c9
Liam Caffrey <liamcaffrey@live.com.pt>
2020-12-01 11:34:21 +0000

    changing some file directories

--- scripts/get_x_train.py
+++
@@ -1,8 +0,0 @@
-import os
-import pickle
-import streamlit as st
-
-def get_var():
-    root_path = os.path.dirname(os.path.dirname(__file__))
-    path_var = os.path.join(root_path,'raw_data','x_train.pkl')
-    return pickle.load(open(path_var,"rb"))
